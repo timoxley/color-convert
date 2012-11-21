f037d436640701c3212988629cea965442baab98
Tim Oxley <secoif@gmail.com>
2012-11-22 04:34:20 +1000

    Tidy jsdoc, readme and methodnames.

--- Readme.md
+++ Readme.md
@@ -8,28 +8,28 @@
 
 ## API
 
-### rgbToHsl()
+### RGBtoHSL()
 
   Converts an RGB color value to HSL. Conversion formula
   adapted from http://en.wikipedia.org/wiki/HSL_color_space.
   Assumes r, g, and b are contained in the set [0, 255] and
   returns h, s, and l in the set [0, 1].
 
-### hslToRgb()
+### HSLtoRGB()
 
   Converts an HSL color value to RGB. Conversion formula
   adapted from http://en.wikipedia.org/wiki/HSL_color_space.
   Assumes h, s, and l are contained in the set [0, 1] and
   returns r, g, and b in the set [0, 255].
 
-### rgbToHsv()
+### RGBtoHSV()
 
   Converts an RGB color value to HSV. Conversion formula
   adapted from http://en.wikipedia.org/wiki/HSV_color_space.
   Assumes r, g, and b are contained in the set [0, 255] and
   returns h, s, and v in the set [0, 1].
 
-### hsvToRgb()
+### HSVtoRGB()
 
   Converts an HSV color value to RGB. Conversion formula
   adapted from http://en.wikipedia.org/wiki/HSV_color_space.
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Source: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
 
-module.exports.rgbToHsl = rgbToHsl
-module.exports.hslToRgb = hslToRgb
-module.exports.rgbToHsv = rgbToHsv
-module.exports.hsvToRgb = hsvToRgb
+module.exports.RGBtoHSL = RGBtoHSL
+module.exports.HSLtoRGB = HSLtoRGB
+module.exports.RGBtoHSV = RGBtoHSV
+module.exports.HSVtoRGB = HSVtoRGB
 
 /**
  * Converts an RGB color value to HSL. Conversion formula
@@ -11,31 +11,32 @@ module.exports.hsvToRgb = hsvToRgb
  * Assumes r, g, and b are contained in the set [0, 255] and
  * returns h, s, and l in the set [0, 1].
  *
- * @param   Number  r       The red color value
- * @param   Number  g       The green color value
- * @param   Number  b       The blue color value
- * @return  Array           The HSL representation
+ * @param {Number} r The red color value
+ * @param {Number} g The green color value
+ * @param {Number} b The blue color value
+ * @return {Array} The HSL representation
+ * @api public
  */
 
-function rgbToHsl(r, g, b){
-    r /= 255, g /= 255, b /= 255;
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, l = (max + min) / 2;
+function RGBtoHSL(r, g, b){
+  r /= 255, g /= 255, b /= 255;
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
 
-    if(max == min){
-        h = s = 0; // achromatic
-    }else{
-        var d = max - min;
-        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-        switch(max){
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
-        }
-        h /= 6;
+  if(max == min){
+    h = s = 0; // achromatic
+  }else{
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch(max){
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
     }
+    h /= 6;
+  }
 
-    return [h, s, l];
+  return [h, s, l];
 }
 
 /**
@@ -44,34 +45,36 @@ function rgbToHsl(r, g, b){
  * Assumes h, s, and l are contained in the set [0, 1] and
  * returns r, g, and b in the set [0, 255].
  *
- * @param   Number  h       The hue
- * @param   Number  s       The saturation
- * @param   Number  l       The lightness
- * @return  Array           The RGB representation
+ * @param {Number} h The hue
+ * @param {Number} s The saturation
+ * @param {Number} l The lightness
+ * @return {Array} The RGB representation
+ * @api public
  */
-function hslToRgb(h, s, l){
-    var r, g, b;
 
-    if(s == 0){
-        r = g = b = l; // achromatic
-    }else{
-        function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-            return p;
-        }
+function HSLtoRGB(h, s, l){
+  var r, g, b;
 
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1/3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
+  if(s == 0){
+    r = g = b = l; // achromatic
+  }else{
+    function hue2rgb(p, q, t){
+      if(t < 0) t += 1;
+      if(t > 1) t -= 1;
+      if(t < 1/6) return p + (q - p) * 6 * t;
+      if(t < 1/2) return q;
+      if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
     }
 
-    return [r * 255, g * 255, b * 255];
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return [r * 255, g * 255, b * 255];
 }
 
 /**
@@ -80,31 +83,33 @@ function hslToRgb(h, s, l){
  * Assumes r, g, and b are contained in the set [0, 255] and
  * returns h, s, and v in the set [0, 1].
  *
- * @param   Number  r       The red color value
- * @param   Number  g       The green color value
- * @param   Number  b       The blue color value
- * @return  Array           The HSV representation
+ * @param {Number} r The red color value
+ * @param {Number} g The green color value
+ * @param {Number} b The blue color value
+ * @return {Array} The HSV representation
+ * @api public
  */
-function rgbToHsv(r, g, b){
-    r = r/255, g = g/255, b = b/255;
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, v = max;
 
-    var d = max - min;
-    s = max == 0 ? 0 : d / max;
+function RGBtoHSV(r, g, b){
+  r = r/255, g = g/255, b = b/255;
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, v = max;
 
-    if(max == min){
-        h = 0; // achromatic
-    }else{
-        switch(max){
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
-        }
-        h /= 6;
+  var d = max - min;
+  s = max == 0 ? 0 : d / max;
+
+  if(max == min){
+    h = 0; // achromatic
+  }else{
+    switch(max){
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
     }
+    h /= 6;
+  }
 
-    return [h, s, v];
+  return [h, s, v];
 }
 
 /**
@@ -113,28 +118,30 @@ function rgbToHsv(r, g, b){
  * Assumes h, s, and v are contained in the set [0, 1] and
  * returns r, g, and b in the set [0, 255].
  *
- * @param   Number  h       The hue
- * @param   Number  s       The saturation
- * @param   Number  v       The value
- * @return  Array           The RGB representation
+ * @param {Number} h The hue
+ * @param {Number} s The saturation
+ * @param {Number} v The value
+ * @return {Array} The RGB representation
+ * @api public
  */
-function hsvToRgb(h, s, v){
-    var r, g, b;
 
-    var i = Math.floor(h * 6);
-    var f = h * 6 - i;
-    var p = v * (1 - s);
-    var q = v * (1 - f * s);
-    var t = v * (1 - (1 - f) * s);
+function HSVtoRGB(h, s, v){
+  var r, g, b;
 
-    switch(i % 6){
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
-    }
+  var i = Math.floor(h * 6);
+  var f = h * 6 - i;
+  var p = v * (1 - s);
+  var q = v * (1 - f * s);
+  var t = v * (1 - (1 - f) * s);
 
-    return [r * 255, g * 255, b * 255];
+  switch(i % 6){
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+  }
+
+  return [r * 255, g * 255, b * 255];
 }
